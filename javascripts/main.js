ef9cac05f64b6b9b2d0d3d4093baa1fc75a79e1e
MaxWeinreich <max.weinreich@yale.edu>
2016-08-30 20:56:28 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -78,18 +78,4 @@ $(function() {
         };
       })(i));
   };
-  
-  var $sidebar2 = $(".navig");
-  
-  for (var i = 0; i < NumberOfConfigurations; i++) {
-    var item = '<a>' + (i + 1) + '</a>';
-    var locate = $(".td" + points[i].length);
-    $(item)
-      .appendTo ( locate )
-      .click((function(_i) {
-        return function(e) {
-          render(_i);
-        };
-      })(i));
-  };
 });
