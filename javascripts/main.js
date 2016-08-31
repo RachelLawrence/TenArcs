291a79bfe6ba57f06b02c942f4216c64581585bb
MaxWeinreich <max.weinreich@yale.edu>
2016-08-30 20:57:54 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -69,7 +69,7 @@ $(function() {
 
   var $sidebar = $(".nav-pills");
   for (var i = 0; i < NumberOfConfigurations; i++) {
-    var item =(i + 1);
+    var item = "<a>" + (i + 1) + "</a>";
     $(item)
       .appendTo($sidebar)
       .click((function(_i) {
